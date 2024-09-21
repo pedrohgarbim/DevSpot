4d3e5aa9c76d0c3d52cfa5ba3e300ded9432ef25
pedrohgarbim <phg.garbim@gmail.com>
2024-09-21 16:37:18 -0300

    Refactoring the user seeder class

--- DevSpot/Data/UserSeeder.cs
+++ DevSpot/Data/UserSeeder.cs
@@ -9,20 +9,35 @@ namespace DevSpot.Data
 		{
 			var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
-			if (await userManager.FindByEmailAsync("admin@devspot.com") == null)
+			await CreateUserWithRole(userManager, "admin@devspot.com", "Admin123!", Roles.Admin);
+			await CreateUserWithRole(userManager, "jobseeker@devspot.com", "Jobseeker123!", Roles.JobSeeker);
+			await CreateUserWithRole(userManager, "employer@devspot.com", "Employer123!", Roles.Employer);
+
+		}
+
+		public static async Task CreateUserWithRole(UserManager<IdentityUser>
+			userManager, string email, string password, string role)
+			// now the method is 100% dynamic 
+		{
+			if (await userManager.FindByEmailAsync(email) == null)
 			{
 				var user = new IdentityUser
 				{
-					Email = "admin@devspot.com",
+					Email = email,
 					EmailConfirmed = true,
-					UserName = "admin@devspot.com"
+					UserName = email
 				};
 
-				var result = await userManager.CreateAsync(user, "Admin123!");
+				var result = await userManager.CreateAsync(user, password);
 
 				if (result.Succeeded)
 				{
-					await userManager.AddToRoleAsync(user, Roles.Admin);
+					await userManager.AddToRoleAsync(user, role);
+				}
+				else
+				{
+					throw new Exception($"Failed creating user with email {user.Email}. Errors: " +
+						$"{string.Join(",", result.Errors)}");
 				}
 			}
 		}
