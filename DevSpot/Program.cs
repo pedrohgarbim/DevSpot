1985696a916de3eac5fbdd458b115652c9d66a6f
pedrohgarbim <phg.garbim@gmail.com>
2024-09-21 15:46:50 -0300

    seeding the admin role but in a wrong way

--- DevSpot/Program.cs
+++ DevSpot/Program.cs
@@ -29,6 +29,11 @@ using (var scope = app.Services.CreateScope())
 {
 	var services = scope.ServiceProvider;
 	var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();	
+
+	if (!roleManager.RoleExistsAsync("Admin").Result)
+	{
+		var result = roleManager.CreateAsync(new IdentityRole("Admin")).Result;
+	}
 }
 
 
