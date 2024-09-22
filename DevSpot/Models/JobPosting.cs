486578a0e3ea21ae26bef1f1e2369023470c074b
pedrohgarbim <phg.garbim@gmail.com>
2024-09-22 14:24:31 -0300

    Job Posting Model

---
+++ DevSpot/Models/JobPosting.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpot.Models
+{
+    public class JobPosting
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+    }
+}
