9befa3061b8898dbc56d3126089a7a13e090824f
pedrohgarbim <phg.garbim@gmail.com>
2024-09-22 13:48:47 -0300

    registration process

--- DevSpot/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DevSpot/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -97,6 +97,8 @@ namespace DevSpot.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            public bool IsJobSeeker { get; set; } = true;
         }
 
 
